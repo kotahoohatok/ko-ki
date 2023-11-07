450c19c55455f6f045bff324ef179f5eea8d9ef8
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-11-07 09:31:35 +0900

    Update ex06-88.js

--- ex06-88.js
+++ ex06-88.js
@@ -1,1 +1,1 @@
-function gazo688(){document.getElementByld('who688').innerHTML='<img src="./image/josai2.jpg" alt="オン">';}
+function gazo688(){document.getElementByld('who688').innerHTML='<img src="image/josai2.jpg" alt="オン">';}
