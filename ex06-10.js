4c447a2dde94c82c0eea629faa747d45efeb9748
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-11-07 10:09:20 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -1,2 +1,2 @@
-function imgchange1(){document.getElementByld('logo').src='image/josai2.jpg';}
-function imgchange2(){document.getElementByld('logo').src='image/josai1.jpg';}
+function imgchange1(){document.getElementById('logo').src='image/josai2.jpg';}
+function imgchange2(){document.getElementById('logo').src='image/josai1.jpg';}
