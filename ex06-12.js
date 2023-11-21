6f151473b0ae051a9fbbc14556fd6115d238438a
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-11-21 10:06:16 +0900

    Update ex06-12.js

--- ex06-12.js
+++ ex06-12.js
@@ -7,7 +7,9 @@ function hyoji(){
 if(cnt<11){
   //現在より1文字多く切り出して表示 
   document.timer.moji.value=str.substr(0,cnt+1);}　
-
+else{
+//すべて表示されたら、空文字に戻す
+  document.timer.moji.value="";}}
 function startfnc(){//関数hyoji()を1000ミリ秒間隔で呼び出す setInterval("hyoji()",1000);}
 
 const str='Mozilla';
