f75f6159569e64cb3c6bbd29a4ad81a607ea889f
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-11-21 10:02:55 +0900

    Update ex06-12.js

--- ex06-12.js
+++ ex06-12.js
@@ -10,6 +10,7 @@ if(cnt<11){
 else{
   //すべて表示されたら、空文字に戻す 　
   document.timer.moji.value="";}}
+
 function startfnc(){//関数hyoji()を1000ミリ秒間隔で呼び出す setInterval("hyoji()",1000);}
 
 const str='Mozilla';
