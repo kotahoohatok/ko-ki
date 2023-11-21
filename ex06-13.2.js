374e16d536ede9d04702588378f1917715771050
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-11-21 09:37:22 +0900

    Update ex06-13.2.js

--- ex06-13.2.js
+++ ex06-13.2.js
@@ -1,4 +1,4 @@
-function hyoj13(){//表示する文字　
+function hyoji13(){//表示する文字　
   var str13="一文字ずつ表示します。";//テキストボックスの文字数　
   var cnt13=document.timer13.moji13.value.length;//文字が全部表示されているか確認　
 if(cnt13<11){//現在より一文字多く切り出して表示　
