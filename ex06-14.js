6ef5ed1ecfbd25124e212036026ebe3bf1871438
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-11-28 09:42:05 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,6 +1,4 @@
 function imgchange1(){document.getElementById('timer').src='image/josai2.jpg';}
 function imgchange2(){document.getElementById('timer').src='image/josai1.jpg';}
 
-function mes(){}
-
-settimeout(){}
+settimeout2("imgchange1()",3000);}
