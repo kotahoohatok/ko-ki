546687d2c22ce331cb1a6a6571b93be1813b4b29
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-12-12 10:06:03 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,9 +1,3 @@
-function bmi5(){ var x,y
-    x = document.getElementById('bmi1').value;
-    y = document.getElementById('bmi2').value;
-    alert(Number(x) / Number(y) * Number(y));}
-
-
 function keisan(){ //keisan　という関数を実行する
   var a, b,BMI;
   a = document.getElementById("shi").value;
