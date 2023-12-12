60ee5288a64220d6e906903531b0009bfe3cda2a
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-12-12 10:07:21 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,6 +1,6 @@
 function keisan(){ //keisan　という関数を実行する
   var a, b,BMI;
-  a = document.getElementById("shi").value;
+  a = document.getElementById("sin").value;
   b	= document.getElementById("tai").value; 
   BMI = b/( a*a ); 
 　alert('あなたのボディマス指数（BMI）は' + BMI + 'です' );}
