b20298d4caa70e16c17108fe7b797eb34bea7f47
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-12-12 09:42:59 +0900

    Create ex07-3.js

---
+++ ex07-3.js
@@ -0,0 +1,12 @@
+function bmi5(){ var x,y
+    x = document.getElementById('bmi1').value;
+    y = document.getElementById('bmi2').value;
+    alert(Number(x) / Number(y) * Number(y));}
+
+
+function keisan(){ //keisan　という関数を実行する
+  var a, b,d;
+  a = document.getElementById("shincho").value;
+  b	= document.getElementById("taijuu").value; 
+  c = b/( (a/100)*(a/100) ); //BMIの計算結果をcに代入
+　alert('あなたのボディマス指数（BMI）は' + c + 'です' );}
