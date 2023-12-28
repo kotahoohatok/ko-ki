2e4d53be2ae39f90140a55e7bb6498a78c2975d9
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-12-28 18:45:21 +0900

    Update ex07-11.js

--- ex07-11.js
+++ ex07-11.js
@@ -1,8 +1,8 @@
 function hantei() {
     var X = Number(document.getElementById('uru').value);
     if ((X % 4 == 0 && X % 100 != 0) || (X % 400 == 0)) {
-        alert('西暦' + X + '年は閏年');
+        alert('西暦' + X + '年は閏年です。');
     } else {
-        alert('西暦' + X + '年は平年');
+        alert('西暦' + X + '年は平年です。');
     }
 }
