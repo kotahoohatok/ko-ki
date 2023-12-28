0a0c7c13b953a9e2634c8c1e5e51fbadb5cec900
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-12-28 15:16:42 +0900

    Create ex07-9.js

---
+++ ex07-9.js
@@ -0,0 +1,11 @@
+function nazo3() {
+    if (document.getElementById('answer3').value == '夢') {
+        alert('あたり！');
+    } 
+    else if (document.getElementById('answer3').value == 'ゆめ') {
+        alert('あたり！');
+    } 
+    else {
+        alert('はずれ！');
+    }
+}
