e4805a066aa9b3188b26c0ad237b9dd14c90289d
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-12-28 15:18:13 +0900

    Create ex07-11.js

---
+++ ex07-11.js
@@ -0,0 +1,8 @@
+function uru() {
+    var X = Number(document.getElementById('seireki').value);
+    if ((X % 4 == 0 && X % 100 != 0) || (X % 400 == 0)) {
+        alert('西暦' + X + '年は閏年');
+    } else {
+        alert('西暦' + X + '年は平年');
+    }
+}
